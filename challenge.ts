225a87fda1a832881ba5a6fed01b4ce90987ef38
Peter May <peter.may@winzana.com>
2022-03-14 16:41:58 +0100

    base files

---
+++ challenge.ts
@@ -0,0 +1,18 @@
+interface User{
+  name: string;
+  age: number
+}
+const prettyPrintWilder = (users: User[]) => {
+    users.map((user: User) => {
+      console.log(`${user.name} is ${user.age} years old`);
+    });
+  };
+  
+  const wilders: User[] = [];
+  const user1: User = { name: "Pierre", age: 23 };
+  const user2: User = { name: "Paul", age: (new Date().getFullYear() - new Date("10/02/1990").getFullYear() )};
+  const user3: User = { name: "Jacques", age: 25 };
+  wilders.push(user1);
+  wilders.push(user2);
+  wilders.push(user3);
+  prettyPrintWilder(wilders);
